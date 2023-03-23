95c9a7d982c26d19a4e43405240a5e3094fe03ca
MR AĞA <99508889+tecnoagateam@users.noreply.github.com>
2023-03-23 13:04:32 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -85,15 +85,8 @@ anlik_calisan = []
 
 tekli_calisan = []
   
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
 
-app = Client("GUNC",
-             api_id=api_id,
-             api_hash=api_hash,
-             bot_token=bot_token
-             )	
+
 	
 	
 @client.on(events.NewMessage(pattern="^/start$"))
