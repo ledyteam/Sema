b8160ff8b66375638164b245cb0254941a5f3182
MR AĞA <99508889+tecnoagateam@users.noreply.github.com>
2023-07-28 22:51:50 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config():
     API_ID = int(os.environ.get("API_ID","18052289"))
     API_HASH = os.environ.get("API_HASH","552525f45a3066fee54ca7852235c19c")
     BOT_TOKEN = os.environ.get("BOT_TOKEN","5192558668:AAHLhH7gCYZ8nKnmTL-zsvTL3qqINdqgbo0")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","Sematagbot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "NezaretRobot")
     BOT_NAME = os.environ.get("BOT_NAME","𝗦Σ𝗠Δ 𝗧Δ𝗚𝗚Σ𝗥")
     BOT_ID = int(os.environ.get("BOT_ID","6118669969"))
     SUDO_USERS = os.environ.get("SUDO_USERS","1924693109").split()
